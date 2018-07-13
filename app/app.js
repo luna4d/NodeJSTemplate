02d7f3df2a3b5d6cf9af28f440fcef2bbbb54312
Nightly <nightly@outlook.kr>
2018-07-13 11:38:25 +0900

    .

---
+++ app/app.js
@@ -0,0 +1,9 @@
+const root=module.parent.root; // like C# using keyword
+
+class app{
+	static main(){
+
+	}
+}
+
+module.exports=app;
---
+++ app/program.js
@@ -0,0 +1,10 @@
+// program : referance list like C#
+const
+	program={
+		root:require('./root')
+	,	app:require('./app')
+	};
+
+module.exports=program;
+
+program.app.main(); // entry point
